a4c0b5539503ef7d012696902baf8a09a418a57d
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-05 11:49:17 +0300

    Убрал лишний вывод в консоль

--- CountTheNumberOfIntegersWithSameDigits.cpp
+++ CountTheNumberOfIntegersWithSameDigits.cpp
@@ -12,7 +12,6 @@ int main()
 	SetConsoleOutputCP(1251);
 
 	int start = 100, end = 999, counter = 0;
-	int store[3];
 
 	for (int i = start; i <= end; i++)
 	{
@@ -20,19 +19,12 @@ int main()
 		int tens = (i / 10) % 10;
 		int units = i % 10;
 
-		if ((units == tens && tens != hundreds) || (hundreds == tens && units != hundreds))
-		{
+		if ((units == tens && tens != hundreds) || (hundreds == tens && units != hundreds))		
 			counter++;
-			cout << i << endl;
-		}
 			
 	}
 
 	cout << "Всего количество целых чисел в диапазоне от 100 до 999, у которых есть две одинаковые цифры " << counter;
 }
-
-//    
-//
-
 
 
